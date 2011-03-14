cc5e07e2cdff137eda050d81bff421cb62dcefc9
Matt Owen <mowen@iq.harvard.edu>
2011-03-14 16:35:40 -0400

    Sweave/LaTeX/Bibliography Changes

---
+++ R/describe.ologit.R
@@ -0,0 +1,8 @@
+describe.ologit <- function (...) {
+  list(
+       author = c("Matt Owen", "Kosuke Imai", "Olivia Lau", "Gary King"),
+       model = "ologit",
+       title = "Ordinal Probit Regression for Ordered Categorical Dependent Variables",
+       year = 2011
+      )
+}
---
+++ R/describe.oprobit.R
@@ -0,0 +1,9 @@
+describe.oprobit <- function (...) {
+  list(
+       author = c("Matt Owen", "Kosuke Imai", "Olivia Lau", "Gary King"),
+       model = "oprobit",
+       title = "Ordinal Logit Regression for Ordered Categorical Dependent Variables",
+       year = 2011
+      )
+}
+
